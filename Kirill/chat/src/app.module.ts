f4ee8fbde8468945d61ffdf6eecbbe6ebc6dfc5c
Kirill <kirill108bol@gmail.com>
2024-01-22 18:43:42 -0500

    delete module auth

--- Kirill/chat/src/app.module.ts
+++ Kirill/chat/src/app.module.ts
@@ -6,11 +6,17 @@ import { ChatModule } from './chat/chat.module';
 import { MessageModule } from './message/message.module';
 import { PollingModule } from './polling/polling.module';
 import { RedisModule } from './modules/redis.module';
-import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [RedisModule, UserModule, ChatModule, MessageModule, PollingModule, AuthModule, MailModule],
+  imports: [
+    RedisModule,
+    UserModule,
+    ChatModule,
+    MessageModule,
+    PollingModule,
+    MailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
